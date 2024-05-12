88a45a8f4e396f00c1539f0f94e8bada9ea97154
maaurissio <maur.gajardo@duocuc.cl>
2024-05-11 23:48:43 -0400

    ola

---
+++ Curso-Cisco/3.4.10.py
@@ -0,0 +1,36 @@
+from os import system
+system("cls")
+
+# Los Beatles fueron uno de los grupos de música más populares de la década de 1960 y la banda más vendida en la historia. 
+# Algunas personas los consideran el acto más influyente de la era del rock. 
+# De hecho, se incluyeron en la compilación de la revista Time de las 100 personas más influyentes del siglo XX.
+
+# La banda sufrió muchos cambios de formación, que culminaron en 1962 con la formación de John Lennon, 
+# Paul McCartney, George Harrison y Richard Starkey (mejor conocido como Ringo Starr).
+
+
+# Escribe un programa que refleje estos cambios y le permita practicar con el concepto de listas. Tu tarea es:
+
+# paso 1: crea una lista vacía llamada beatles;
+# paso 2: emplea el método append() para agregar los siguientes miembros de la banda a la lista: John Lennon, Paul McCartney y George Harrison;
+# paso 3: emplea el buclefor y el append() para pedirle al usuario que agregue los siguientes miembros de la banda a la lista: Stu Sutcliffe, y Pete Best;
+# paso 4: usa la instrucción del para eliminar a Stu Sutcliffe y Pete Best de la lista;
+# paso 5: usa el método insert() para agregar a Ringo Starr al principio de la lista.
+# Por cierto, ¿eres fan de los Beatles? (Los Beatles son una de las bandas favoritas de Greg. Pero espera...¿Quién es Greg?)
+
+beatles = []
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("Goerge Harrison")
+print(beatles)
+
+for i in range(2):
+    miembros = input("Agregue un miembro nuevo: ")
+    beatles.append(miembros)
+    print(beatles)
+del beatles[3]
+print(beatles)
+del beatles[3]
+print(beatles)
+beatles.insert(0, "Ringo Starr")
+print(beatles)
