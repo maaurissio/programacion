e0b4c45bb3ea352ea05e45dd567423716c68dc1a
maaurissio <maur.gajardo@duocuc.cl>
2024-06-19 16:29:57 -0400

    Inicio ejercicio bus

---
+++ ejercicio bus/am.py
@@ -0,0 +1,18 @@
+from os import system
+system("cls")
+
+"""
+-Se requiere desarrollar una aplicacion en python para la empresa de transporte Universo. 
+
+-Esta aplicacion debe gestionar las reservas de pasajes para buses con capacidad para 16 personas.
+
+-El programa debe registrar por cada pasaje el rut del pasajero, y consultar si desea ventana o pasillo.
+
+-La aplicacion debe mostrar los asientos disponibles en un menu (segun el tipo, ventana o pasillo) para hacer la reserva.
+
+-El programa tras cada reserva, debe generar un archivo de texto con el boleto.
+
+-El programa debe permitir la generacion de archivo CSV con todas las reservas.
+
+-La aplicacion debe incluir una opcion para salir de ella.
+"""
